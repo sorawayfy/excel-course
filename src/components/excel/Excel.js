c9544c8778a0cefc7fc57204745da88d51d26db4
sorawayfy <sorawayfy@gmail.com>
2020-07-29 16:50:27 +0300

    Finish base framework functional

---
+++ src/components/excel/Excel.js
@@ -0,0 +1,33 @@
+import {$} from '@/core/dom'
+
+export class Excel {
+    constructor(selector, options) {
+        // eslint-disable-next-line no-undef
+        this.$el = $(selector)
+        this.components = options.components || []
+    }
+
+    getRoot() {
+        const $root = $.create('div', 'excel')
+        this.components = this.components.map(Component => {
+            const $el = $.create('div', Component.className)
+            const component = new Component($el)
+            // DEBUG
+            if (component.name) {
+                // eslint-disable-next-line no-undef
+                window['c' + component.name] = component
+            }
+            $el.html(component.toHTML())
+            $root.append($el)
+            return component
+        })
+
+        return $root
+    }
+
+    render() {
+        this.$el.append(this.getRoot())
+
+        this.components.forEach(component => component.init())
+    }
+}
