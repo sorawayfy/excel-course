5392b5c4ded72eae643959cc0f2efdfde41381d1
sorawayfy <sorawayfy@gmail.com>
2020-08-05 15:56:48 +0300

    Begin the logic

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,17 +1,24 @@
 import {$} from '@/core/dom'
+import {Emitter} from '@core/Emitter'
 
 export class Excel {
     constructor(selector, options) {
         // eslint-disable-next-line no-undef
         this.$el = $(selector)
         this.components = options.components || []
+        this.emitter = new Emitter()
     }
 
     getRoot() {
         const $root = $.create('div', 'excel')
+
+        const componentOptions = {
+            emitter: this.emitter
+        }
+
         this.components = this.components.map(Component => {
             const $el = $.create('div', Component.className)
-            const component = new Component($el)
+            const component = new Component($el, componentOptions)
             // // DEBUG
             // if (component.name) {
             //     // eslint-disable-next-line no-undef
@@ -29,5 +36,9 @@ export class Excel {
         this.$el.append(this.getRoot())
 
         this.components.forEach(component => component.init())
+    }
+
+    destroy() {
+        this.components.forEach(component => component.destroy())
     }
 }
